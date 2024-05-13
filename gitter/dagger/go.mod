955aff83b087eb88497405a3cd8ffb4b325b2fe9
Siddhartha Basu <biosidd@gmail.com>
2024-05-13 17:09:37 -0500

    feat(gitter): add new Gitter module for handling git operations
    This new module provides a Gitter struct with methods for setting the
    repository name, git reference, and repository path. It also includes a
    Checkout method for cloning a repository at the specified path and
    reference. This addition will allow for more streamlined and organized
    handling of git operations within the project.
    The dagger.json file provides necessary configuration for the Gitter
    module, including its name, SDK, source, and engine version. This
    configuration is essential for the proper functioning of the module.

--- gitter/dagger/go.mod
+++ gitter/dagger/go.mod
@@ -1,10 +1,11 @@
-module dagger/git
+module dagger/gitter
 
 go 1.22.2
 
 require (
 	github.com/99designs/gqlgen v0.17.44
 	github.com/Khan/genqlient v0.7.0
+	github.com/go-git/go-git/v5 v5.12.0
 	github.com/vektah/gqlparser/v2 v2.5.11
 	go.opentelemetry.io/otel v1.24.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.24.0
@@ -26,7 +27,6 @@ require (
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
 	github.com/go-git/go-billy/v5 v5.5.0 // indirect
-	github.com/go-git/go-git/v5 v5.12.0 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
@@ -38,7 +38,6 @@ require (
 	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
 	github.com/skeema/knownhosts v1.2.2 // indirect
 	github.com/sosodev/duration v1.2.0 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.24.0 // indirect
 	go.opentelemetry.io/otel/metric v1.24.0 // indirect
---
+++ gitter/dagger/main.go
@@ -0,0 +1,120 @@
+// A generated module for Gitter functions
+
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+)
+
+type Gitter struct {
+	// Repository name
+	Repository string
+	// Git reference
+	Ref        string
+	// Repository path
+	Path       string
+}
+
+// WithPath sets the path where the repository should be placed.
+func (gcmd *Gitter) WithPath(
+	// path to place the repository
+	// +optional
+	// +default="current working directory"
+	path string,
+) (*Gitter, error) {
+	if len(path) == 0 {
+		curr, err := os.Getwd()
+		if err != nil {
+			return gcmd, fmt.Errorf(
+				"error in getting current working dir %q",
+				err,
+			)
+		}
+		path = curr
+	}
+	gcmd.Path = path
+	return gcmd, nil
+}
+
+// WithRef sets the Git reference (branch, tag, or SHA)
+func (gcmd *Gitter) WithRef(
+	// the branch, tag or sha to checkout, Required.
+	ref string,
+) (*Gitter, error) {
+	if len(ref) == 0 {
+		return gcmd, errors.New("ref value is required")
+	}
+	gcmd.Ref = ref
+	return gcmd, nil
+}
+
+// WithRepository sets the GitHub repository name
+func (gcmd *Gitter) WithRepository(
+	// github repository name with owner, for example tora/bora, Required
+	repository string,
+) (*Gitter, error) {
+	if len(repository) == 0 {
+		return gcmd, errors.New("repository value is required")
+	}
+	gcmd.Repository = repository
+	return gcmd, nil
+}
+
+// Checkout clones a repository at the specified path and reference
+func (gcmd *Gitter) Checkout(ctx context.Context) (*Gitter, error) {
+	_, err := cloneRepo(gcmd.Path, gcmd.Ref, gcmd.Repository)
+	if err != nil {
+		return nil, err
+	}
+	return gcmd, nil
+}
+
+func cloneRepo(path, ref, repository string) (*git.Repository, error) {
+	repo, err := git.PlainClone(path, false, &git.CloneOptions{
+		URL:           repository,
+		ReferenceName: plumbing.ReferenceName(ref),
+	})
+	if err == nil {
+		return repo, nil
+	}
+	if !errors.Is(err, plumbing.ErrReferenceNotFound) {
+		return nil, fmt.Errorf("error in checking out repo %q", err)
+	}
+	return cloneDefaultBranch(path, repository, ref)
+}
+
+func cloneDefaultBranch(path, repository, ref string) (*git.Repository, error) {
+	repo, err := git.PlainClone(
+		path,
+		false,
+		&git.CloneOptions{URL: repository},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error in checking out default branch %q", err)
+	}
+	return checkoutRef(repo, repository, ref)
+}
+
+func checkoutRef(
+	repo *git.Repository,
+	repository, ref string,
+) (*git.Repository, error) {
+	wtree, err := repo.Worktree()
+	if err != nil {
+		return nil, fmt.Errorf(
+			"error in getting worktree from default branch %q",
+			err,
+		)
+	}
+	err = wtree.Checkout(&git.CheckoutOptions{Hash: plumbing.NewHash(ref)})
+	if err != nil {
+		return nil, fmt.Errorf("error in checking out ref %s %q", ref, err)
+	}
+	return repo, nil
+}
