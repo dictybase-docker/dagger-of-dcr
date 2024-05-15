66b8a55d32013ded2feaab530889bdba2b9a69b1
Siddhartha Basu <biosidd@gmail.com>
2024-05-14 19:47:41 -0500

    refactor(main.go): simplify Gitter struct and Checkout method
    The Gitter struct and Checkout method were simplified to improve
    readability and maintainability. The WithPath method was removed as it
    was unnecessary and the Checkout method was refactored to use the
    dag.Git() method for cloning and checking out a branch.

--- gitter/dagger/main.go
+++ gitter/dagger/main.go
@@ -5,11 +5,7 @@ package main
 import (
 	"context"
 	"errors"
-	"fmt"
-	"os"
-
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
+	"strings"
 )
 
 type Gitter struct {
@@ -19,27 +15,6 @@ type Gitter struct {
 	Ref string
 	// Repository path
 	Path string
-}
-
-// WithPath sets the path where the repository should be placed.
-func (gcmd *Gitter) WithPath(
-	// path to place the repository
-	// +optional
-	// +default="current working directory"
-	path string,
-) (*Gitter, error) {
-	if len(path) == 0 {
-		curr, err := os.Getwd()
-		if err != nil {
-			return gcmd, fmt.Errorf(
-				"error in getting current working dir %q",
-				err,
-			)
-		}
-		path = curr
-	}
-	gcmd.Path = path
-	return gcmd, nil
 }
 
 // WithRef sets the Git reference (branch, tag, or SHA)
@@ -66,27 +41,12 @@ func (gcmd *Gitter) WithRepository(
 	return gcmd, nil
 }
 
-// Checkout clones a repository at the specified path and reference
-func (gcmd *Gitter) Checkout(ctx context.Context) (*Directory, error) {
-	_, err := cloneRepo(gcmd.Path, gcmd.Ref, gcmd.Repository)
-	if err != nil {
-		return nil, err
-	}
-	return dag.Directory().Directory(gcmd.Path), nil
-}
-
-func cloneRepo(path, ref, repository string) (*git.Repository, error) {
-	repo, err := git.PlainClone(path, false, &git.CloneOptions{
-		URL:           repository,
-		ReferenceName: plumbing.ReferenceName(ref),
-	})
-	if err == nil {
-		return repo, nil
-	}
-	if !errors.Is(err, plumbing.ErrReferenceNotFound) {
-		return nil, fmt.Errorf("error in checking out repo %q", err)
-	}
-	return cloneDefaultBranch(path, repository, ref)
+// Checkout clones the repository, checks out a specified branch, and returns the directory.
+func (gcmd *Gitter) Checkout(ctx context.Context) *Directory {
+	return dag.Git().
+		Clone(gcmd.Repository).
+		Checkout(parseBranchName(gcmd.Ref)).
+		Directory()
 }
 
 // Inspect clones the given repository and returns a Terminal instance for inspection
