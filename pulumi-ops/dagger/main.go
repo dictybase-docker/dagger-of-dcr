6b785237f383fdd6a724405e73ddc7c2da28d13e
Siddhartha Basu <biosidd@gmail.com>
2024-06-13 15:23:26 -0500

    feat(dagger/main.go): add builder methods to PulumiOps for enhanced configurability
    Added methods `WithPulumi`, `WithBackend`, `WithCredentials`, and
    `WithKubeConfig` to the `PulumiOps` struct in `main.go`. These methods
    allow setting the Pulumi version, backend, credentials, and Kubernetes
    configuration respectively.

--- pulumi-ops/dagger/main.go
+++ pulumi-ops/dagger/main.go
@@ -12,3 +12,44 @@ type PulumiOps struct {
 	KubeConfig  *File
 	Credentials *File
 }
+
+// WithPulumi sets the Pulumi version for the PulumiOps instance.
+func (pmo *PulumiOps) WithPulumi(
+	ctx context.Context,
+	// pulumi version
+	// + default="3.108.0"
+	version string,
+) *PulumiOps {
+	pmo.Version = version
+	return pmo
+}
+
+// WithBackend sets the backend for storing state for the PulumiOps instance.
+func (pmo *PulumiOps) WithBackend(
+	ctx context.Context,
+	// pulumi backend for storing state, required
+	backend string,
+) *PulumiOps {
+	pmo.Backend = backend
+	return pmo
+}
+
+// WithCredentials sets the credentials file for the PulumiOps instance.
+func (pmo *PulumiOps) WithCredentials(
+	ctx context.Context,
+	// credentials file(google cloud for the time being), required
+	credentials *File,
+) *PulumiOps {
+	pmo.Credentials = credentials
+	return pmo
+}
+
+// WithKubeConfig sets the Kubernetes configuration file for the PulumiOps instance.
+func (pmo *PulumiOps) WithKubeConfig(
+	ctx context.Context,
+	// kubernetes configuration file, required
+	config *File,
+) *PulumiOps {
+	pmo.KubeConfig = config
+	return pmo
+}
