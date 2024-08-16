d3a054fb8a2cc80a4e1af5b3d411ec72436c1b43
Siddhartha Basu <biosidd@gmail.com>
2024-08-16 16:30:19 -0500

    feat(main.go): enhance documentation and add new methods for ArangoDB integration
    1. Updated the documentation for existing methods (`Test`, `Lint`, `Publish`) to provide more detailed descriptions and parameter explanations, improving clarity and usability for developers.
    2. Introduced new methods (`TestsWithArangoDB`, `WithArangoPassword`, `WithArangoVersion`, `WithArangoPort`, `WithGolangVersion`) to support testing with an ArangoDB service. This allows developers to run integration tests involving a database, making the testing process more robust and closer to production scenarios.
    3. The new methods also include configuration setters for ArangoDB, such as setting the version, port, and password, providing flexibility in testing different configurations and environments.

--- golang/dagger/main.go
+++ golang/dagger/main.go
@@ -22,13 +22,20 @@ type Golang struct {
 	GolangVersion  string
 }
 
-// Runs golang tests
+// Test runs Go tests in a containerized environment.
+// It sets up a Wolfi-based container with the specified Go version,
+// prepares the workspace, and executes the tests.
+//
+// Parameters:
+//   - ctx: The context for the operation.
+//   - src: The source directory to test (required).
+//   - args: Optional slice of strings representing additional arguments to the go test command.
+//
+// Returns:
+//   - A string containing the stdout of the test execution.
+//   - An error if any step in the process fails.
 func (gom *Golang) Test(
 	ctx context.Context,
-	// An optional Go version to use for testing
-	// +optional
-	// +default="go-1.21"
-	version string,
 	// The source directory to test, Required.
 	src *Directory,
 	// An optional slice of strings representing additional arguments to the go test command
@@ -38,14 +45,25 @@ func (gom *Golang) Test(
 	return F.Pipe5(
 		dag.Container(),
 		base(WOLFI_BASE),
-		wolfiWithGoInstall(version),
+		wolfiWithGoInstall(gom.GolangVersion),
 		prepareWorkspace(src, PROJ_MOUNT),
 		modCache,
 		goTestRunner(args),
 	).Stdout(ctx)
 }
 
-// Lint runs golangci-lint on the Go source code
+// Lint runs golangci-lint on the Go source code in a containerized environment.
+// It uses a specified version of golangci-lint to perform static code analysis.
+//
+// Parameters:
+//   - ctx: The context for the operation.
+//   - version: Optional string specifying the version of golangci-lint to use (default: "v1.55.2-alpine").
+//   - src: The source directory to lint (required).
+//   - args: Optional slice of strings representing additional arguments to the golangci-lint command.
+//
+// Returns:
+//   - A string containing the stdout of the linting process.
+//   - An error if any step in the process fails.
 func (gom *Golang) Lint(
 	ctx context.Context,
 	// An optional string specifying the version of golangci-lint to use
@@ -68,6 +86,20 @@ func (gom *Golang) Lint(
 }
 
 // Publish builds and pushes a Docker image to a Docker registry.
+// It uses the provided Docker context and Dockerfile to build the image,
+// then pushes it to the specified Docker registry.
+//
+// Parameters:
+//   - ctx: The context for the operation.
+//   - src: The source directory where the Docker context is located (optional, default: ".").
+//   - namespace: The Docker namespace under which the image will be pushed (optional, default: "dictybase").
+//   - dockerFile: The path to the Dockerfile (optional, default: "build/package/Dockerfile").
+//   - image: The name of the image to be built (required).
+//   - imageTag: The tag of the image to be built (required).
+//
+// Returns:
+//   - A string containing the result of the publish operation.
+//   - An error if any step in the process fails.
 func (gom *Golang) Publish(
 	ctx context.Context,
 	// specifies the source directory where the Docker context is located
@@ -110,6 +142,62 @@ func fetchAndValidateEnvVars(envVar string) (string, error) {
 	}
 	return value, nil
 }
+
+// TestsWithArangoDB runs Go tests in a containerized environment with an ArangoDB service.
+// It sets up an ArangoDB service, prepares a Wolfi-based container with the specified Go version,
+// and executes the tests with the ArangoDB service available.
+//
+// Parameters:
+//   - ctx: The context for the operation.
+//   - src: The source directory to test (required).
+//   - args: Optional slice of strings representing additional arguments to the go test command.
+//
+// Returns:
+//   - A string containing the stdout of the test execution.
+//   - An error if any step in the process fails.
+func (gom *Golang) TestsWithArangoDB(
+	ctx context.Context,
+	// The source directory to test, Required.
+	src *Directory,
+	// An optional slice of strings representing additional arguments to the go test command
+	// +optional
+	args []string,
+) (string, error) {
+	arangoService := dag.Container().
+		From(gom.ArangoVersion).
+		WithEnvVariable("ARANGO_ROOT_PASSWORD", gom.ArangoPassword).
+		WithExposedPort(gom.ArangoPort).
+		AsService()
+
+	arangoHost, err := arangoService.Hostname(ctx)
+	if err != nil {
+		return arangoHost, fmt.Errorf(
+			"error in retrieving arangodb host %w",
+			err,
+		)
+	}
+
+	return dag.Container().From(WOLFI_BASE).
+		WithServiceBinding("arango", arangoService).
+		WithEnvVariable("ARANGO_HOST", arangoHost).
+		WithEnvVariable("ARANGO_PASS", gom.ArangoPassword).
+		WithEnvVariable("ARANGO_USER", "root").
+		WithExec([]string{"apk", "update"}).
+		WithExec([]string{"apk", "add", gom.GolangVersion}).
+		WithMountedDirectory(PROJ_MOUNT, src).
+		WithWorkdir(PROJ_MOUNT).
+		WithExec([]string{"go", "mod", "download"}).
+		WithExec(append([]string{"go", "test", "-v", "./..."}, args...)).
+		Stdout(ctx)
+}
+
+// WithArangoPassword sets the root password for the ArangoDB instance.
+//
+// Parameter:
+//   - password: The root password for the ArangoDB instance (optional, default: "golam").
+//
+// Returns:
+//   - A pointer to the modified Golang struct.
 func (gom *Golang) WithArangoPassword(
 	// The root password for the ArangoDB instance
 	// +optional
@@ -120,6 +208,13 @@ func (gom *Golang) WithArangoPassword(
 	return gom
 }
 
+// WithArangoVersion sets the version of ArangoDB to use.
+//
+// Parameter:
+//   - version: The version of ArangoDB to use (optional, default: "3.10.9").
+//
+// Returns:
+//   - A pointer to the modified Golang struct.
 func (gom *Golang) WithArangoVersion(
 	// The version of ArangoDB to use
 	// +optional
@@ -130,6 +225,13 @@ func (gom *Golang) WithArangoVersion(
 	return gom
 }
 
+// WithArangoPort sets the port to expose ArangoDB on.
+//
+// Parameter:
+//   - port: The port to expose ArangoDB on (optional, default: 8529).
+//
+// Returns:
+//   - A pointer to the modified Golang struct.
 func (gom *Golang) WithArangoPort(
 	// The port to expose ArangoDB on
 	// +optional
@@ -139,6 +241,14 @@ func (gom *Golang) WithArangoPort(
 	gom.ArangoPort = port
 	return gom
 }
+
+// WithGolangVersion sets the version of Golang to use.
+//
+// Parameter:
+//   - version: The version of Golang to use (optional, default: "go-1.21").
+//
+// Returns:
+//   - A pointer to the modified Golang struct.
 func (gom *Golang) WithGolangVersion(
 	// The version of Golang to use
 	// +optional
