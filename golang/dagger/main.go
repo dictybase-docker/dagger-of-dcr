7204d846e298fbcb7b6efef0c83d2e355af18746
Siddhartha Basu <biosidd@gmail.com>
2024-08-21 13:41:28 -0500

    refactor(golang): extract Redis-related functions to separate file for better modularity
    By moving Redis-related functions to a new file `redis.go`, the codebase
    becomes more modular, making it easier to maintain and understand.

--- golang/dagger/main.go
+++ golang/dagger/main.go
@@ -188,63 +188,6 @@ func (gom *Golang) WithGotestSumFormatter(
 	return gom
 }
 
-// TestsWithRedis runs Go tests in a container with Redis.
-func (gom *Golang) TestsWithRedis(
-	ctx context.Context,
-	// The source directory to test, Required.
-	src *Directory,
-	// An optional slice of strings representing additional arguments to the go test command
-	// +optional
-	args []string,
-) (string, error) {
-	redisService := dag.Container().
-		From(fmt.Sprintf("redis:%s-alpine", gom.RedisVersion)).
-		WithExposedPort(gom.RedisPort).
-		AsService()
-
-	redisHost, err := redisService.Hostname(ctx)
-	if err != nil {
-		return redisHost, fmt.Errorf(
-			"error in retrieving redis host %w",
-			err,
-		)
-	}
-
-	return gom.PrepareTestContainer(ctx).
-		WithServiceBinding("redis", redisService).
-		WithEnvVariable("REDIS_SERVICE_HOST", redisHost).
-		WithEnvVariable("REDIS_SERVICE_PORT", fmt.Sprintf("%d", gom.RedisPort)).
-		WithMountedDirectory(PROJ_MOUNT, src).
-		WithWorkdir(PROJ_MOUNT).
-		WithExec([]string{"go", "mod", "download"}).
-		WithExec(append([]string{
-			"gotestsum", "--format", gom.GotestSumFormatter, "--",
-		}, args...)).
-		Stdout(ctx)
-}
-
-// WithRedisVersion sets the version of Redis to use.
-func (gom *Golang) WithRedisVersion(
-	// The version of Redis to use
-	// +optional
-	// +default="7.0.12"
-	version string,
-) *Golang {
-	gom.RedisVersion = version
-	return gom
-}
-
-// WithRedisPort sets the port to expose Redis on.
-func (gom *Golang) WithRedisPort(
-	// The port to expose Redis on
-	// +optional
-	// +default=6379
-	port int,
-) *Golang {
-	gom.RedisPort = port
-	return gom
-}
-
 // PrepareTestContainer creates a container with Golang and installs gotestsum and gotestdox
 func (gom *Golang) PrepareTestContainer(
 	ctx context.Context,
