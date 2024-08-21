b7d804a1ecc3d295d344f9d4912b1dd2a787fdf5
Siddhartha Basu <biosidd@gmail.com>
2024-08-21 13:31:12 -0500

    feat(main.go): add Redis integration for Go testing framework
    Added Redis configuration fields to the Golang struct to support Redis
    in the testing environment. Introduced new methods `TestsWithRedis`,
    `WithRedisVersion`, and `WithRedisPort` to configure and utilize Redis
    during Go tests.

--- golang/dagger/main.go
+++ golang/dagger/main.go
@@ -20,6 +20,9 @@ type Golang struct {
 	ArangoPassword     string
 	ArangoVersion      string
 	ArangoPort         int
+	RedisPassword      string
+	RedisVersion       string
+	RedisPort          int
 	GolangVersion      string
 	GotestSumFormatter string
 }
@@ -182,6 +185,63 @@ func (gom *Golang) WithGotestSumFormatter(
 	formatter string,
 ) *Golang {
 	gom.GotestSumFormatter = formatter
+	return gom
+}
+
+// TestsWithRedis runs Go tests in a container with Redis.
+func (gom *Golang) TestsWithRedis(
+	ctx context.Context,
+	// The source directory to test, Required.
+	src *Directory,
+	// An optional slice of strings representing additional arguments to the go test command
+	// +optional
+	args []string,
+) (string, error) {
+	redisService := dag.Container().
+		From(fmt.Sprintf("redis:%s-alpine", gom.RedisVersion)).
+		WithExposedPort(gom.RedisPort).
+		AsService()
+
+	redisHost, err := redisService.Hostname(ctx)
+	if err != nil {
+		return redisHost, fmt.Errorf(
+			"error in retrieving redis host %w",
+			err,
+		)
+	}
+
+	return gom.PrepareTestContainer(ctx).
+		WithServiceBinding("redis", redisService).
+		WithEnvVariable("REDIS_SERVICE_HOST", redisHost).
+		WithEnvVariable("REDIS_SERVICE_PORT", fmt.Sprintf("%d", gom.RedisPort)).
+		WithMountedDirectory(PROJ_MOUNT, src).
+		WithWorkdir(PROJ_MOUNT).
+		WithExec([]string{"go", "mod", "download"}).
+		WithExec(append([]string{
+			"gotestsum", "--format", gom.GotestSumFormatter, "--",
+		}, args...)).
+		Stdout(ctx)
+}
+
+// WithRedisVersion sets the version of Redis to use.
+func (gom *Golang) WithRedisVersion(
+	// The version of Redis to use
+	// +optional
+	// +default="7.0.12"
+	version string,
+) *Golang {
+	gom.RedisVersion = version
+	return gom
+}
+
+// WithRedisPort sets the port to expose Redis on.
+func (gom *Golang) WithRedisPort(
+	// The port to expose Redis on
+	// +optional
+	// +default=6379
+	port int,
+) *Golang {
+	gom.RedisPort = port
 	return gom
 }
 
