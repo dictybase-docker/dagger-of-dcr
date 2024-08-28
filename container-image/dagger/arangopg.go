86327aaa1154993bdb3299fb95716a3e035b73d9
Siddhartha Basu <biosidd@gmail.com>
2024-08-28 16:26:09 -0500

    feat(arangopg.go): add functionality to create and publish ArangoPostgres container
    This new functionality in `arangopg.go` allows the creation of a
    container that combines ArangoDB and PostgreSQL, along with the backup
    tool restic, on a linux/amd64 platform. It also includes methods to
    build and publish this container to a Docker registry.

---
+++ container-image/dagger/arangopg.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+// CreateArangoPostgresContainer creates a container based on ArangoDB 3.10.14,
+// updates it, installs PostgreSQL 14, and restic on the linux/amd64 platform
+func (cmg *ContainerImage) CreateArangoPostgresContainer(
+	ctx context.Context,
+) (*Container, error) {
+	return dag.Container(ContainerOpts{Platform: "linux/amd64"}).
+			From("arangodb:3.10.14").
+			WithExec([]string{"apk", "update"}).
+			WithExec([]string{"apk", "add", "postgresql14", "curl", "bzip2"}).
+			WithExec([]string{"curl", "-L", "https://github.com/restic/restic/releases/download/v0.17.0/restic_0.17.0_linux_amd64.bz2", "-o", "/tmp/restic_0.17.0_linux_amd64.bz2"}).
+			WithExec([]string{"bunzip2", "/tmp/restic_0.17.0_linux_amd64.bz2"}).
+			WithExec([]string{"mv", "/tmp/restic_0.17.0_linux_amd64", "/usr/local/bin/restic"}).
+			WithExec([]string{"chmod", "+x", "/usr/local/bin/restic"}).
+			WithExec([]string{"rm", "-f", "/tmp/restic_0.17.0_linux_amd64.bz2"}),
+		nil
+}
+
+// BuildAndPublishArangoPostgresContainer builds and publishes the ArangoPostgres container
+func (cmg *ContainerImage) BuildAndPublishArangoPostgresContainer(
+	ctx context.Context,
+	// dockerhub user name
+	user string,
+	// dockerhub password, use an api token
+	password string,
+) (string, error) {
+	container, err := cmg.CreateArangoPostgresContainer(ctx)
+	if err != nil {
+		return "", fmt.Errorf(
+			"error creating ArangoPostgres container: %w",
+			err,
+		)
+	}
+
+	tag := fmt.Sprintf("%s/%s:%s", cmg.Namespace, cmg.Image, cmg.DockerImageTag)
+	_, err = container.
+		WithRegistryAuth(
+			"docker.io",
+			user,
+			dag.SetSecret("docker-pass", password),
+		).
+		Publish(ctx, tag)
+	if err != nil {
+		return "", fmt.Errorf(
+			"error publishing ArangoPostgres container: %w",
+			err,
+		)
+	}
+
+	return tag, nil
+}
